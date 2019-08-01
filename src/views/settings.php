d7e12e8ace13b24dddf089d41a27e0322ad7d87b
Pera Burek <peraburek@gmail.com>
2019-08-01 14:41:45 +0000

    typo fixed

--- src/views/settings.php
+++ src/views/settings.php
@@ -1,5 +1,5 @@
 <h2>Refresh Page Settings</h2>
-<p>Here you can configure how many seconds of inactivity triggers page refrsh</p>
+<p>Here you can configure how many seconds of inactivity triggers page refersh</p>
 <form method="post" action="<?= esc_html(admin_url('admin-post.php')) ?>">
     Refresh after how many seconds?<br>
     <input type="number" name="seconds" placeholder="<?= isset($this->settings['seconds']) ? (int) $this->settings['seconds'] : 60 ?>"><br>
